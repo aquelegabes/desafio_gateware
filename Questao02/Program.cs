7a72545aa9e506ce0e6023f5e8678f0256a52981
Gabriel Santos <galexander.santos1@gmail.com>
2019-11-26 21:06:26 -0300

    Continuacao commit anterior

--- Questao02/Program.cs
+++ Questao02/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Questao02
 {
@@ -6,7 +10,33 @@ namespace Questao02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string assemblyPath = Path.GetFullPath(Assembly.GetEntryAssembly().Location);
+            var filesPath = assemblyPath.Replace("bin/Debug/netcoreapp2.2/Questao02.dll", "Arquivos");
+
+            // declarando numeros
+            List<string> telefones = new List<string>();
+
+            // lendo arquivos
+            foreach (var file in Directory.EnumerateFiles(filesPath, "*.txt"))
+            {
+                string contents = File.ReadAllText(file);
+                var conteudos = contents.Split(';');
+                if (conteudos == null)
+                    break;
+                foreach (var item in conteudos)
+                {
+                    var isValid = item.Contains("+");
+                    isValid = isValid && item.Substring(1,item.Length-1).All(char.IsDigit);
+                    isValid = isValid && item.Length >= 9;
+                    if (isValid == false) break;
+
+                    telefones.Add(item);
+                }
+            }
+
+            System.Console.WriteLine($"Numeros encontrados {telefones?.Count ?? 0}");
+            if (telefones?.Count >= 1)
+                telefones.ForEach(System.Console.WriteLine);
         }
     }
 }
--- Questao03/Program.cs
+++ Questao03/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace Questao03
 {
-    class Program
+    public class Program
     {
+        public static bool IsLinux
+        {
+            get 
+            {
+                int p = (int) Environment.OSVersion.Platform;
+                return (p == 4) || (p == 6) || (p == 128);
+            }
+        }
+
+        public static void CriarDiretorio(out string path)
+        {
+            path = IsLinux ? "/home/copias/" : "C:/copias/";
+            System.Console.WriteLine("Criando diretório onde as imagens serão armazenadas...");
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+
+        public static void CopiarDe(string fromPath, string wherePath)
+        {
+            if (string.IsNullOrWhiteSpace(fromPath) || string.IsNullOrWhiteSpace(wherePath))
+                throw new ArgumentNullException();
+
+            if (Directory.Exists(fromPath))
+            {
+                var images = Directory.EnumerateFiles(fromPath);
+
+                foreach (var img in images)
+                {
+                    var fileName = img.Split('/').Last();
+                    var file = File.ReadAllBytes(img);
+                    File.WriteAllBytes(wherePath + fileName, file);
+                }
+            }
+            System.Console.WriteLine("Arquivos salvos!");
+        }
+        // Iniciar como administrador
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CriarDiretorio(out string path);
+
+            System.Console.WriteLine("Digite um diretório para copiar as imagens:");
+            string copyFrom = Console.ReadLine();
+
+            CopiarDe(copyFrom, path);
         }
     }
 }
